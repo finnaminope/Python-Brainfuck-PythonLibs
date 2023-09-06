d5108a1d2b2c39c0b05d0dc1603a246c8ecd112a
finnaminope <91102755+finnaminope@users.noreply.github.com>
2023-09-06 14:58:08 -0600

    Add Comments

--- brainfuck.py
+++ brainfuck.py
@@ -18,41 +18,45 @@ def evaluate(code):
   code     = cleanup(list(code))
   bracemap = buildbracemap(code)
 
-  cells, codeptr, cellptr, codeExecList = [0], 0, 0, []
+  cells, codeptr, cellptr, codeExecList, queue = [0], 0, 0, [], []
   while codeptr < len(code):
     command = code[codeptr]
 
-    if command == ">":
-      cellptr += 1
-      if cellptr == len(cells): cells.append(0)
+    if not comment:
+      if command == ">":
+        cellptr += 1
+        if cellptr == len(cells): cells.append(0)
 
-    if command == "<":
-      cellptr = 0 if cellptr <= 0 else cellptr - 1
+      if command == "<":
+        cellptr = 0 if cellptr <= 0 else cellptr - 1
 
-    if command == "+":
-      cells[cellptr] = cells[cellptr] + 1 if cells[cellptr] < 255 else 0
+      if command == "+":
+        cells[cellptr] = cells[cellptr] + 1 if cells[cellptr] < 255 else 0
 
-    if command == "-":
-      cells[cellptr] = cells[cellptr] - 1 if cells[cellptr] > 0 else 255
+      if command == "-":
+        cells[cellptr] = cells[cellptr] - 1 if cells[cellptr] > 0 else 255
 
-    if command == "/":
-      filename = sys.argv[1]
-      print("Program Trace (ID: " + str(cellptr) + "-" + str(cells[cellptr]) + "-" + str(codeptr) + ") At " + str(codeptr) + " in " + filename + ". This Trace Was Not Called Because of An Error In The Program.")
-      print("This is the Program Code That Was Executed: ")
-      for item in codeExecList:
-        print(item, end="")
+      if command == "/":
+        filename = sys.argv[1]
+        print("Program Trace (ID: " + str(cellptr) + "-" + str(cells[cellptr]) + "-" + str(codeptr) + ") At " + str(codeptr) + " in " + filename + ". This Trace Was Not Called Because of An Error In The Program.")
+        print("This is the Program Code That Was Executed: ")
+        for item in codeExecList:
+          print(item, end="")
 
-    if command == "[" and cells[cellptr] == 0: codeptr = bracemap[codeptr]
-    if command == "]" and cells[cellptr] != 0: codeptr = bracemap[codeptr]
-    if command == ".": sys.stdout.write(chr(cells[cellptr]))
-    if command == ",": cells[cellptr] = ord(getch.getch())
+      if command == "[" and cells[cellptr] == 0: codeptr = bracemap[codeptr]
+      if command == "]" and cells[cellptr] != 0: codeptr = bracemap[codeptr]
+      if command == ".": sys.stdout.write(chr(cells[cellptr]))
+      if command == ",": cells[cellptr] = ord(getch.getch())
     
-    codeptr += 1
-    codeExecList.append(command)
+      codeptr += 1
+      codeExecList.append(command)
+
+    if command == "(": comment = True
+    if command == ")": comment = False
 
 
 def cleanup(code):
-  return ''.join(filter(lambda x: x in ['.', ',', '[', ']', '<', '>', '+', '-', '/'], code))
+  return ''.join(filter(lambda x: x in ['.', ',', '[', ']', '<', '>', '+', '-', '/', 'i', 'o'], code))
 
 
 def buildbracemap(code):
